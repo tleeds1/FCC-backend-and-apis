59d2a0b8b6fadb97f08229039510ba18083445f0
tleeds1 <thodoanle26@gmail.com>
2024-09-07 23:41:50 +0700

    File Upload Metadata Project

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
 require('dotenv').config()
-
 var app = express();
 
 app.use(cors());
@@ -11,7 +11,16 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+var upload = multer({ dest: 'uploads/' });
 
+app.post('/api/fileanalyse',  upload.single('upfile'), function (req, res) {
+  if (!req.file) {
+    return res.json({ error: 'No file uploaded' });
+  }
+  res.json({ name: req.file.originalname,
+             type: req.file.mimetype,
+             size: req.file.size});
+});
 
 
 const port = process.env.PORT || 3000;
